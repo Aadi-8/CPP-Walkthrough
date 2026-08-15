ab343b1fb3b05689f382525227888e22d032f271
Aadi-8 <raiaman0705@gmil.com>
2026-08-15 14:43:25 +0530

    Day1: Data types and I/O

---
+++ demo.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x >>y;
+    //int,long,long long for storing integer of diffrent ranges
+    int a = 78;
+    long b = 400;
+    long long c = 30000;
+    //float,double for decimals and can store numbers 
+    float d = 3.6;
+    double e = 3.99471;
+    cout << "value is:" << x << " and other is :" << y;
+    cout << "\na is "<<a <<  "\nb is " << b << "\nc is " << c <<"\nfloat d is "<< d <<"\ndouble e is "<< e; ; 
+
+    // strings and getline for strings,lines etc.
+    string s1 , s2 ;
+    cin >> s1 >> s2;
+    cout << "\n"<<s1 << " "<< s2 <<"\n"; //here we need to add a string variable for every word after a space
+    string str ;
+    cin.ignore();                  //throws the leftover \n 
+    getline(cin,str);              //Tips: Always add cin.ignore before jumping on getline
+    cout << str;                   //getline read till the end of the line
+
+    char ee='@';                   // to store single character 
+    cout << ee;
+    return 0;
+}
