c21873a66a66ba6a4f8a7f42c8f1baacb54a2c45
Aadi-8 <raiaman0705@gmail.com>
2026-08-26 14:35:02 +0530

    Sum & Factorial of N numbers.

--- Basics.cpp
+++ Basics.cpp
@@ -1,21 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n = 0;
-string s;
-void name(){
-    if (n==5){
-        return;
+
+void name(int i , int n ){
+    if (i>n) return;
+    cout << "Aadi" << endl;
+    name(i+1,n);
+    
+}
+void num(int i , int n ){
+    if (i>n) return;
+    cout << i << endl;
+    num(i+1,n);
+}
+void revnum(int i){
+    if (i==0) return;
+    cout << i << endl;
+    revnum(i-1);
+}
+void Add(int i, int sum){
+    if (i<1){
+        cout << sum <<endl;
+    return;
     }
-    else{
-        cout << s << endl;
-    }
-    n++;
-    name ();
+    Add(i-1,sum+i);
+}
+int Add2(int i){                 //If we return number we cannot add in void (int + void)
+    if (i==0) return 0 ;
+    return i + Add2(i-1);
+}
+int factorial(int i){
+    if (i < 1) return 1 ;
+    return i * factorial(i-1);
 }
 int main(){
-    cout << "Entre: ";
-    cin >> s;
-    
-    name();
-   
+    name(1,4);
+    num(1,4);
+    revnum(4);
+    Add(4,0);
+    cout << Add2(3) << endl;
+    cout << factorial(4) << endl;
+    return 0;
 }
