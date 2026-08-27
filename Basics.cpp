e7412ea54df5a4f8ee3fd0f9bbf88f68646a60a3
Aadi-8 <raiaman0705@gmail.com>
2026-08-27 17:40:06 +0530

    Pallindrome check

--- Basics.cpp
+++ Basics.cpp
@@ -22,15 +22,33 @@ void Add(int i, int sum){
         cout << sum <<endl;
     return;
     }
-    Add(i-1,sum+i);
+   Add(i-1,sum+i);
 }
-int Add2(int i){                 //If we return number we cannot add in void (int + void)
+int Add2(int i){                 //If we return number, we cannot add in void (int + void)
     if (i==0) return 0 ;
     return i + Add2(i-1);
 }
 int factorial(int i){
     if (i < 1) return 1 ;
     return i * factorial(i-1);
+}
+void reverse1(vector<int>& arr,int i , int n){
+    if (i>=n) {
+       return ;
+    }
+    swap(arr[i],arr[n]);
+    reverse1(arr,i+1,n-1);
+}
+bool pallindrome(const string &s ,int i, int n){
+    if (i>=n) {
+        cout << "It is a pallindrome" << endl ; 
+        return true ;
+    }
+    if (s[i]!=s[n]){
+         cout << "Not a pallindrome";
+         return false;
+    }
+    return pallindrome(s,i+1,n-1);
 }
 int main(){
     name(1,4);
@@ -39,5 +57,25 @@ int main(){
     Add(4,0);
     cout << Add2(3) << endl;
     cout << factorial(4) << endl;
+    // int arraysize;
+    // cout << "Entre array size: ";
+    // cin >> arraysize;
+    // vector<int> arr1(arraysize);
+    //  for(int k=0;k<arraysize;k++){
+    //         cout << "Fill indexes " << k << ": ";
+    //         cin >> arr1[k];
+    //     }
+    // reverse1(arr1,0,arraysize-1);
+    //  for(int k=0;k<arraysize;k++){
+    //         cout << arr1[k] << " ";
+    //     }
+    //     cout << endl;
+    string d;
+    cout << "Entre your pallindrome string ";
+    cin >> d;
+  
+    int len=d.size();
+    int a=0;
+    pallindrome(d,a,len-1);
     return 0;
 }
