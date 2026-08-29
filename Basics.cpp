7dc1ac785b0a20dd85dc159915628654c882452d
Aadi-8 <raiaman0705@gmail.com>
2026-08-30 00:16:29 +0530

    Hashing

--- Basics.cpp
+++ Basics.cpp
@@ -56,13 +56,32 @@ int fabonaci(int n){
     }
      return fabonaci(n-1)+fabonaci(n-2);
 }
+void sub(int ind,vector<int> &ds,int arr[],int n){
+    if (ind == n){
+        for (auto tt:ds) {
+        cout << tt << " ";
+    }
+
+   if(ds.size()==0){
+    cout << "{}";
+   }
+    cout << endl;
+    return;
+}
+
+    sub(ind+1,ds,arr,n);
+    ds.push_back(arr[ind]);
+    sub(ind+1,ds,arr,n);
+    ds.pop_back();
+    return;
+}
 int main(){
     name(1,4);
     num(1,4);
     revnum(4);
     Add(4,0);
     cout << Add2(3) << endl;
-    cout << factorial(4) << endl;
+    cout << "Factorial "<< factorial(4) << endl;
     // int arraysize;
     // cout << "Entre array size: ";
     // cin >> arraysize;
@@ -82,6 +101,22 @@ int main(){
     int len=d.size();
     int a=0;
     pallindrome(d,a,len-1);
-    cout << fabonaci(4) << endl;
+    cout << "Fabonaci"<< fabonaci(4) << endl;
+
+    int arr2[] = {1,5,3};
+    int aaa=3;
+    vector<int> ds;
+    sub(0,ds,arr2,aaa);
+
+    //Hashing
+    int n=5;                              //We can take input or declare array
+    int arr3[n] = {1,2,1,3,12};
+    int hash[13] = {0};                   //Hash is not a function just another array
+    for (int i=0; i<n ;i++){
+        hash[arr3[i]] += 1;
+    }                                     //we can use loop to ask the inputs
+    cout << "1 appear" << hash[1] <<"times" << endl;
+    cout << "12 appear" << hash[12] <<"times" << endl;
+
     return 0;
 }
